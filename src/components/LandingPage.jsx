8b781a5bbce9506d1b704f3f6ec66c26ca9dc61b
Wavxr <johnveraguilar25@gmail.com>
2025-04-16 00:47:50 +0800

    chore: update assets

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,10 @@
 function LandingPage() {
-    return (
-      <div>
-        <h2>Landing Page Component</h2>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <img src="/logo.png" alt="RawLens Logo" width="150" />
+      <h2>Landing Page Component</h2>
+    </div>
+  )
+}
   
   export default LandingPage
