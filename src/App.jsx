4dec829aea25fe58c6335430f091b17a4ed6dd07
Wavxr <johnveraguilar25@gmail.com>
2025-07-28 00:35:19 +0800

    refactor(app): added inclusions page

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,65 @@
-// src/App.jsx
-import { Routes, Route } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
-import RedirectRoute from './components/RedirectRoute';
+  // src/App.jsx
+  import { Routes, Route } from 'react-router-dom';
+  import ProtectedRoute from './components/ProtectedRoute';
+  import RedirectRoute from './components/RedirectRoute';
 
-// Pages
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import NotFound from './pages/NotFound';
+  // Pages
+  import Landing from './pages/Landing';
+  import Login from './pages/Login';
+  import Signup from './pages/Signup';
+  import NotFound from './pages/NotFound';
 
-// Admin
-import AdminDashboard from './pages/admin/AdminDashboard';
-import AdminCameras from './pages/admin/Cameras';
-import AdminUsers from './pages/admin/Users';
+  // Admin
+  import AdminDashboard from './pages/admin/AdminDashboard';
+  import AdminCameras from './pages/admin/Cameras';
+  import AdminUsers from './pages/admin/Users';
+  import AdminInclusions from './pages/admin/Inclusions';
 
-// User
-import UserDashboard from './pages/user/UserDashboard';
-import UserCameras from './pages/user/Cameras';
-import UserRent from './pages/user/Rent';
-import UserEducational from './pages/user/Educational';
-import UserProfile from './pages/user/Profile';
+  // User
+  import UserDashboard from './pages/user/UserDashboard';
+  import UserCameras from './pages/user/Cameras';
+  import UserRent from './pages/user/Rent';
+  import UserEducational from './pages/user/Educational';
+  import UserProfile from './pages/user/Profile';
 
-function App() {
-  return (
-    <Routes>
-      {/* Public Page */}
-      <Route path="/" element={<Landing />} />
+  function App() {
+    return (
+      <Routes>
+        {/* Public Page */}
+        <Route path="/" element={<Landing />} />
 
-      {/* Redirect logged-in users away from login/signup */}
-      <Route element={<RedirectRoute />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Route>
-
-      {/* Admin Routes (only accessible to logged-in admin) */}
-      <Route element={<ProtectedRoute requiredRole="admin" />}>
-        <Route path="/admin" element={<AdminDashboard />}>
-          <Route index element={<AdminCameras />} />
-          <Route path="cameras" element={<AdminCameras />} />
-          <Route path="users" element={<AdminUsers />} />
+        {/* Redirect logged-in users away from login/signup */}
+        <Route element={<RedirectRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Route>
-      </Route>
 
-      {/* User Routes (only accessible to logged-in user) */}
-      <Route element={<ProtectedRoute requiredRole="user" />}>
-        <Route path="/user" element={<UserDashboard />}>
-          <Route index element={<UserCameras />} />
-          <Route path="cameras" element={<UserCameras />} />
-          <Route path="rent" element={<UserRent />} />
-          <Route path="educational" element={<UserEducational />} />
-          <Route path="profile" element={<UserProfile />} />
+        {/* Admin Routes (only accessible to logged-in admin) */}
+        <Route element={<ProtectedRoute requiredRole="admin" />}>
+          <Route path="/admin" element={<AdminDashboard />}>
+            <Route index element={<AdminCameras />} />
+            <Route path="cameras" element={<AdminCameras />} />
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="inclusions" element ={<AdminInclusions/>} />
+          </Route>
         </Route>
-      </Route>
 
-      {/* Catch-all for 404 Not Found */}
-      <Route path="*" element={<NotFound />} />
+        {/* User Routes (only accessible to logged-in user) */}
+        <Route element={<ProtectedRoute requiredRole="user" />}>
+          <Route path="/user" element={<UserDashboard />}>
+            <Route index element={<UserCameras />} />
+            <Route path="cameras" element={<UserCameras />} />
+            <Route path="rent" element={<UserRent />} />
+            <Route path="educational" element={<UserEducational />} />
+            <Route path="profile" element={<UserProfile />} />
+          </Route>
+        </Route>
 
-    </Routes>
-  );
-}
+        {/* Catch-all for 404 Not Found */}
+        <Route path="*" element={<NotFound />} />
 
-export default App;
+      </Routes>
+    );
+  }
+
+  export default App;
